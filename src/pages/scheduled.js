a9946935a010b9e15db3529220d9d8fb94d8690b
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2021-09-05 08:32:56 -0300

    feat: show time block list

--- src/pages/scheduled.js
+++ src/pages/scheduled.js
@@ -1,7 +1,84 @@
-export default function Scheduled() {
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons'
+import { Box, Button, Container, Flex, IconButton, SimpleGrid, Spinner } from '@chakra-ui/react'
+import { useFetch } from '@refetty/react'
+import { addDays, subDays } from 'date-fns'
+
+import { useAuth } from '../components/Auth'
+import { Logo } from '../components/Logo'
+import { FormatDate } from '../components/Date'
+
+const getScheduled = async (when) => {
+  return axios({
+    method: 'get',
+    url: '/api/scheduled',
+    params: { when, username: window.location.pathname }
+  })
+}
+
+export default function Schedule() {
+  const [auth, { logout }] = useAuth()
+  const router = useRouter()
+  const [when, setWhen] = useState(() => new Date())
+  const [data, { loading, status, error }, fetch] = useFetch(getScheduled, { lazy: true })
+
+  function addDay() {
+    setWhen(prevState => addDays(prevState, 1))
+  }
+
+  function removeDay() {
+    setWhen(prevState => subDays(prevState, 1))
+  }
+
+  useEffect(() => {
+    fetch(when)
+  }, [when])
+
   return (
-    <div>
-      Scheduled
-    </div>
+    <Container>
+      <Flex as='header' p='4' align='center' justify='space-between'>
+        <Box w='150px'>
+          <Logo />
+        </Box>
+        <Button onClick={logout}>Sair</Button>
+      </Flex>
+      <Flex align='center' justify='space-between' mt='8'>
+        <IconButton
+          icon={ <ChevronLeftIcon /> }
+          variant='unstyled'
+          onClick={ removeDay }
+        />
+        <Box>{ FormatDate(when, 'PPPP') }</Box>
+        <IconButton
+          icon={ <ChevronRightIcon /> }
+          variant='unstyled'
+          onClick={ addDay }
+        />
+      </Flex>
+      <SimpleGrid p='4' columns='2' spacing='4'>
+        { loading && (
+          <Spinner
+            size='xl'
+            thickness='4'
+            speed='0.65s'
+            emptyColor='gray.200'
+            color='blue.500'
+          />
+        ) }
+        { data?.map(time => (
+          <Button
+            key={ time }
+            colorScheme='blue'
+            p='8'
+            fontSize='2xl'
+            fontWeight='400'
+          >
+            { time }
+          </Button>
+        )) }
+      </SimpleGrid>
+    </Container>
   )
 }
